7aeb67192119761e6c7b254fe8174cad6cad00e6
Sergi Comas <github@sercomi.net>
2020-03-15 10:47:59 +0100

    Update to swift 5.2

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 
 import PackageDescription
 
@@ -11,8 +11,8 @@ let package = Package(
         .library(name: "SimpleJWTMiddleware", targets: ["SimpleJWTMiddleware"]),
     ],
     dependencies: [
-    .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta"),
-    .package(url: "https://github.com/vapor/jwt.git", from: "4.0.0-beta"),
+    .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
+    .package(url: "https://github.com/vapor/jwt.git", from: "4.0.0"),
     ],
     targets: [
         .target(name: "SimpleJWTMiddleware", dependencies: ["Vapor", "JWT"])
