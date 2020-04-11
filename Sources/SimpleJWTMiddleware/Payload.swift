82a5936d49a7fdb7f2b804f35684d0510cbd3244
Sergi Comas <sercomi@gmail.com>
2020-04-11 18:30:53 +0200

    Update id to UUID type

--- Sources/SimpleJWTMiddleware/Payload.swift
+++ Sources/SimpleJWTMiddleware/Payload.swift
@@ -6,12 +6,12 @@ public struct Payload: JWTPayload {
     public let firstname: String?
     public let lastname: String?
     public let email: String
-    public let id: Int
+    public let id: UUID
     public let status: Int = 0
     public let exp: String
     public let iat: String
-    
-    public init(id: Int, email: String) {
+
+    public init(id: UUID, email: String) {
         self.id = id
         self.email = email
         self.firstname = nil
@@ -19,7 +19,7 @@ public struct Payload: JWTPayload {
         self.exp = String(Date().addingTimeInterval(60*60*24).timeIntervalSince1970)
         self.iat = String(Date().timeIntervalSince1970)
     }
-    
+
    public func verify(using signer: JWTSigner) throws {
         let expiration = Date(timeIntervalSince1970: Double(self.exp)!)
         try ExpirationClaim(value: expiration).verifyNotExpired()
